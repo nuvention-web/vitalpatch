c2345d465c05ba24b9d9f76d96dc837364d369e3
Michael Hueschen <michaelhueschen@gmail.com>
2014-01-14 02:03:07 -0600

    Admins can't register in production

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,13 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  # No registration in production
+  if Rails.env.production?
+    devise :database_authenticatable, :recoverable,
+           :rememberable, :trackable, :validatable
+  else
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
+  end
 end
