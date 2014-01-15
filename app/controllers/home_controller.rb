5d3796e553d0a7d8a562f54e7be2213dddf75678
sammysf <samueltoizer2014@u.northwestern.edu>
2014-01-14 21:06:48 -0600

    Added alert for email entry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,11 @@ class HomeController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Successfully added"
+      flash[:notice] = "Successfully added"
+      redirect_to root_path 
     else
-      redirect_to root_path, error: "Could not add: ~s" % @user.errors.full_messages
+      flash[:error] = "Could not add: %s" % @user.errors.full_messages
+      redirect_to root_path
     end
   end
 
