bf720c2f0e108bd9c2b42f916acd3a2152a68036
sammysf <samueltoizer2014@u.northwestern.edu>
2014-01-27 23:59:11 -0600

    Removed more file://// references

--- review/includes/include-1390881174324.js
+++ review/includes/include-1390881174324.js
@@ -1,9 +1,9 @@
-// Sam changed this
+// Sam changed
 // document.write("<script type='text/javascript' charset='utf-8' src='file:////Applications/Prototyper Pro.app/Contents/Resources/Java/resources/_jim/javascript/jim-min.js'></script>");
 // document.write("<script type='text/javascript' charset='utf-8' src='file:////Applications/Prototyper Pro.app/Contents/Resources/Java/resources/_jim/javascript/mobile/ios/ios7/iphone/jim-iphone-min.js'></script>");
 document.write("<script type='text/javascript' charset='utf-8' src='resources/_jim/javascript/jim-min.js'></script>");
 document.write("<script type='text/javascript' charset='utf-8' src='resources/_jim/javascript/mobile/ios/ios7/iphone/jim-iphone-min.js'></script>");
-// End Sam changed this
+// End Sam changed
 
 document.write("<script type='text/javascript' charset='utf-8' src='./resources/scroll-1390881174324.js'></script>");
 document.write("<script type='text/javascript' charset='utf-8' src='./resources/lookup-1390881174324.js'></script>");
