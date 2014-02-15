c03ebf066a1c24fccdc291a0ddbea80b59970f1c
Scott Neaves <scottneaves2014@u.northwestern.edu>
2014-02-15 14:07:10 -0600

    changes

--- getvet/getvet.py
+++ getvet/getvet.py
@@ -24,15 +24,15 @@ def search():
             price = "20"
         elif procedure =="tumor-removal":
             price="50"
-        result = {"name":"The Best Vet", "address":"14 libertyville Rd, Evanston, Illinois", "address_url":"14+libertyville+Rd,+Paris,+France", "phone":"123-432-3456", "price":price}
-        #price = price * 2
-        #results += json.dumps(name="The Vet You Can Bet On", address="7 Fortuitous Circle, Evanston, Illinois", address_url="7+Fortuitous+Circle,+Evanston,+Illinois", phone="987-678-7654", price=price)
-        #price = price * 2.2
-        #results += json.dumps(name="Tim's Animal Hospital", address="12 Sherman Avenue, Evanston, Illinois", address_url="12+Sherman+Avenue,+Evanston,+Illinois", phone="678-543-6789", price=price)
+        result = [{"name":"Bramer Animal Hospital Ltd.", "address":"1021 Davis St, Evanston, IL 60201", "address_url":"1021+Davis+St,+Evanston,+IL+60201", "phone":"123-432-3456", "price":price}]
+        price = price * 2
+        result.append({"name":"Fox Animal Hospital", "address":"2107 Crawford Ave, Evanston, IL 60201", "address_url":"2107+Crawford+Ave,+Evanston,+IL+60201", "phone":"987-678-7654", "price":price})
+        price = price * 2.2
+        results.append({"name":"Skokie Animal Hospital", "address":"7550 Lincoln Ave, Skokie, IL 60077", "address_url":"7550+Lincoln+Ave,+Skokie,+IL+60077", "phone":"678-543-6789", "price":price})
         print result
-        return render_template("index.html", results=result)
+        return render_template("index1.html", results=result)
     if request.method=='GET':
-        return render_template("index.html")
+        return render_template("index1.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
