61c695f3194feff181e59c58486390a64cc54ffc
sammysf <samueltoizer2014@u.northwestern.edu>
2014-02-15 14:19:06 -0600

    More front-end

---
+++ getvet/static/custom.js
@@ -0,0 +1,9 @@
+// $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
+// 	$("#procedure-form").submit();
+// });
+
+$('select.form-control').on('change', function(event) {
+	$(this).parent().submit();
+})
+
+$('select.form-control').
