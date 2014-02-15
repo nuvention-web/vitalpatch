6e2c39513075c9c5c1c80374e3b93072d0464ba8
Scott Neaves <scottneaves2014@u.northwestern.edu>
2014-02-15 14:50:55 -0600

    changed header

--- getvet/getvet.py
+++ getvet/getvet.py
@@ -59,7 +59,7 @@ def search():
         results = [{"name":"Bramer Animal Hospital Ltd.", "address":"1021 Davis St, Evanston, IL 60201", "address_url":"1021+Davis+St,+Evanston,+IL+60201", "phone":"123-432-3456", "price":price}]
         price = str(int(price) * 2)
         results.append({"name":"Fox Animal Hospital", "address":"2107 Crawford Ave, Evanston, IL 60201", "address_url":"2107+Crawford+Ave,+Evanston,+IL+60201", "phone":"987-678-7654", "price":price})
-        price = str(int(price) * 2.2)
+        price = str(int(int(price) * 2.2))
         results.append({"name":"Skokie Animal Hospital", "address":"7550 Lincoln Ave, Skokie, IL 60077", "address_url":"7550+Lincoln+Ave,+Skokie,+IL+60077", "phone":"678-543-6789", "price":price})
         return render_template("index.html", results=results, query=request.form['procedure'])
     if request.method=='GET':
