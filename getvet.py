a1d8968301cbbcbc2aecc2535b7c7aa58c9f0b94
sammysf <samueltoizer2014@u.northwestern.edu>
2014-03-04 00:38:49 -0600

    Name changed to VetCompare

--- getvet.py
+++ getvet.py
@@ -10,7 +10,7 @@ from pygeocoder import Geocoder
 import math
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATAB/ASE_URL'] 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/getvet'
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 30
