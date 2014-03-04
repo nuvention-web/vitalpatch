d4f80ad0153f48c5681b33a74c7110dcce409b86
sammysf <samueltoizer2014@u.northwestern.edu>
2014-03-03 19:00:19 -0600

    Added weight and zip fields and submit button to landing page, changed font of landing page

--- getvet.py
+++ getvet.py
@@ -9,8 +9,8 @@ from pygeocoder import Geocoder
 import math
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/getvet'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/getvet'
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 30
 db = SQLAlchemy(app)
@@ -142,7 +142,7 @@ def index():
 
 @app.route('/search', methods = ['GET'])
 def search():
-    procedure = request.args.get('procedure')
+    procedure = request.args.get('procedure').lower()
     weight = request.args.get('weight')
     procedure = Procedure.query.filter(Procedure.name == procedure.lower()).first()
     businesses = Price.query.filter(Price.procedure_id == procedure.id)      \
