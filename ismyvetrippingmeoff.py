dd1df619883d4748bfb00410bd36c0d2994935c4
Glenn Fellman <GlennFellman2014@u.northwestern.edu>
2014-04-20 16:44:54 -0500

    intial database setup

--- ismyvetrippingmeoff.py
+++ ismyvetrippingmeoff.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.secret_key='iwillneverhavetorecall12032'
+# configure SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 30
+
+db = SQLAlchemy(app)
+
+# models
+class input_prices(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	animal_type = db.Column(db.String(3))
+	weight = db.Column(db.Integer)
+	procedure = db.Column(db.String(80))
+	price = db.Column(db.Float)
+	zip = db.Column(db.Integer)
+	clinic_name = db.Column(db.String(80))
+	clinic_yelp_id = db.Column(db.String(150))
 
 #Routes
 
@@ -9,5 +29,6 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
+	db.create_all()
 	app.run(host='0.0.0.0')
 
