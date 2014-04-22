ee9b4708a807ea547e12a97875414e45141087d9
Scott Neaves <scottneaves2014@u.northwestern.edu>
2014-04-22 11:52:20 -0500

    form now submits hidden vet_name field, in addition to the vet_id param submitted via the clinic-select dropdown

--- ismyvetrippingmeoff/static/js.js
+++ ismyvetrippingmeoff/static/js.js
@@ -41,6 +41,12 @@ $(function() {
     });
 });
 
+//Set hidden field's value to the *name* of the vet clinic that has been selected in the vetname_drpdwn select field
+$('#zipcode').on('change', function() {
+  $('#vet_name').value = $("#vetname_drpdwn option:selected").text()
+});
+
+
 $('#zipcode').typing({
     start: function (event, $elem) {
         //do nothing
@@ -68,44 +74,3 @@ $('#zipcode').typing({
 });
 
 
-/*
-//setup before functions
-var typingTimer;                //timer identifier
-var doneTypingInterval = 1000;  //time in ms, 5 second for example
-
-//on keyup, start the countdown
-$('#zipcode').keyup(function(){
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(doneTyping, doneTypingInterval);
-    console.log("keyup");
-});
-
-//on keydown, clear the countdown 
-$('#zipcode').keydown(function(){
-    clearTimeout(typingTimer);
-    console.log("keydown");
-});
-
-//Ajax function to populate the vet clinic dropdown based on zip code field.
-function doneTyping() {
-    $('#zipcode').bind('click', function() {
-      $.getJSON($SCRIPT_ROOT + '/_get_clinics_in_zipcode', {
-        zipcode: $('#zipcode').val(),
-      }, function(data) {
-        $('#vetname_drpdwn')
-            .find('option')
-            .remove();
-
-        //for each business in the yelp json response, passed here from "get_clinics_in_zipcode" function in flask:
-        for (i=0; i<data['businesses'].length; i++){
-            $("#vetname_drpdwn")
-                .append($("<option></option>")
-                .attr("value", data['businesses'][i]['id'])
-                .text(data['businesses'][i]['name']));
-        }
-      });
-      return false;
-    });
-}
-
-*/
