c8d24b22ef863a612d00982bf358ca0a62286adc
sammysf <samueltoizer2014@u.northwestern.edu>
2014-04-25 11:27:34 -0500

    Added gauge to results page, added interstitial, restructured results page, dynamically add weight field if dog and procedure is a surgery

--- static/js.js
+++ static/js.js
@@ -1,21 +1,23 @@
+// Fade in weight text box if "dog" radio button is selected and a surgery is selected
+function weightFadeIn() {
+    var $animalRadio = $('input[name=cat_dog]:checked');
+    var $procedureSelect = $('select[name=procedure] option:selected'); 
 
-
-//Fade in weight text box if "dog" radio button is selected 
-$("#dog_selector").change(function() {
-    console.log("dog_selector changed")
-    if ($(this).prop("checked")){
-        $("#weight").fadeIn();
-        console.log("fade in")
+    if ($animalRadio.val() == 'dog' && $procedureSelect.parent().attr('label') == 'Surgery') {
+        $('#weight').fadeIn();
     }
+    else {
+        $('#weight').fadeOut();
+    }
+}
+
+// Bind function to change for animal radio and procedure select
+$('input[name=cat_dog]').change(function() {
+    weightFadeIn();
 });
 
-//Fade out weight text box if "cat" radio button is selected.
-$("#cat_selector").change(function() {
-    console.log("cat_selector changed")
-    if ($(this).prop("checked")){
-        $("#weight").fadeOut();
-        console.log("fade out")
-    }
+$('select[name=procedure]').change(function() {
+    weightFadeIn();
 });
 
 //Ajax function to populate the vet clinic dropdown based on zip code field.
@@ -32,11 +34,9 @@ $('#zipcode').keyup(function() {
         //for each business in the yelp json response, passed here from "get_clinics_in_zipcode" function in flask:
         if(data['error']!=undefined){
             $("#zip_error").css("visibility","visible");
-            console.log("fade in");
         }
         else{
             $("#zip_error").css("visibility","hidden");
-            console.log("fade out");
             for (i=0; i<data['businesses'].length; i++){
                 $("#vetname_drpdwn")
                     .append($("<option></option>")
@@ -50,6 +50,49 @@ $('#zipcode').keyup(function() {
     }
 });
 
+// Guage
+function gauge(percentileData) {
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.0', {'packages':['gauge']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawGauge);
+
+    // Actually draw it!
+    function drawGauge() {
+        // Create and populate the data table.  Storing 0 initially so we get fun animation.
+        var data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['You', 0]
+        ]);
+
+        var sum = percentileData['25thPercentile'] + percentileData['median'] + percentileData['75thPercentile'];
+        var borderOne = percentileData['25thPercentile'] / sum * 100;
+        var borderTwo = borderOne + percentileData['median'] / sum * 100;
+
+        var options = {
+            greenFrom: 0, greenTo: borderOne,
+            yellowFrom: borderOne, yellowTo: borderTwo,
+            redFrom: borderTwo, redTo: 100,
+            minorTicks: 5,
+            animation:{
+                duration: 1000,
+                easing: 'out',
+            }
+        };
+
+        // Create and draw the visualization.
+        var gauge = new google.visualization.Gauge($('#gauge')[0]);
+        gauge.draw(data, options);
+
+        // Populate data with real value, redraw gauge.
+        data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['You', percentileData['userPercentile']]
+        ]);
+        gauge.draw(data, options);   
+    }
+}
 
 //This code does an action when typing stops for "delay" number of milliseconds. Not used.
 /*
