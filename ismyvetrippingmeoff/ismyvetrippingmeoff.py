772a38ea16e57e23027ce86a40d0100b781a65b2
Scott Neaves <scottneaves2014@u.northwestern.edu>
2014-04-26 16:39:17 -0500

    fixed the mystery of the zip codes which return no clinis. Now using google's maps javascript API for geocoding instead of pygeocode

--- ismyvetrippingmeoff/ismyvetrippingmeoff.py
+++ ismyvetrippingmeoff/ismyvetrippingmeoff.py
@@ -3,6 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 import rauth # OAuth for Yelp
 from pygeocoder import Geocoder
+from math import erf, sqrt
+import json
 
 app = Flask(__name__)
 
@@ -49,17 +51,32 @@ class input_prices(db.Model):
 	zip = db.Column(db.Integer)
 	clinic_name = db.Column(db.String(80))
 	clinic_yelp_id = db.Column(db.String(150))
+	data_integrity = db.Column(db.Integer)
 
-	def __init__(self, animal_type, procedure, price, zip, clinic_name, clinic_yelp_id = None, weight = None):
+	def __init__(self, animal_type, procedure, price, zip, clinic_name, clinic_yelp_id = None, weight = None, data_integrity = None):
 		self.animal_type = animal_type
-		self.weight=weight
-		self.procedure=procedure
-		self.price=price
-		self.zip=zip
-		self.clinic_name=clinic_name
-		self.clinic_yelp_id=clinic_yelp_id
-      
+		self.weight = weight
+		self.procedure = procedure
+		self.price = price
+		self.zip = zip
+		self.clinic_name = clinic_name
+		self.clinic_yelp_id = clinic_yelp_id
+		self.data_integrity = data_integrity
+
+class waitlist(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	email_address = db.Column(db.String(100))
+
+	def __init__(self, email_address):
+		self.email_address=email_address
+
+	def __repr__(self):
+		return '<Email Address: %r>' % self.email_address
+
 db.create_all()
+
+# Remember the data between interstitial and results page
+newVetData = None
 
 # Yelp
 def get_yelp_results(businessID):
@@ -79,9 +96,13 @@ def get_yelp_results(businessID):
     return data
 
 
+# Calculate cumulative distribution function
+def cdf(x):
+	return (1.0 + erf(x / sqrt(2.0))) / 2.0
+
 # User-Facing Routes
 @app.route('/', methods=['GET','POST'])
-def index():
+def index():	
 	if request.method == "GET":
 		# Grab all topics/procedures for select field
 		topicProcedureDict = {}
@@ -96,48 +117,82 @@ def index():
 
 		return render_template('index.html', topicProcedureDict=topicProcedureDict)
 	else:		
-		# Store their price (shhhhh)
-		new_animal_type = request.form['cat_dog']
-		new_price = request.form['cost']
-		new_weight = request.form['optionsRadios']
-		new_procedure = request.form['procedure']
-		new_zip = request.form['zip code']
-		new_clinic_name = request.form['vet_name']
-		new_clinic_yelp_id = request.form['vet_id']
-		newVetData=input_prices(new_animal_type,new_procedure,new_price,new_zip,new_clinic_name,new_clinic_yelp_id,new_weight)
-		#newVetData=input_prices(new_animal_type,new_weight,new_procedure,new_price,new_zip,new_clinic_name,new_clinic_yelp_id)
+		# Delete their previous price, add the data integrity, re-store it
+		db.session.delete(newVetData)
+		newVetData.data_integrity = request.form['integrity']
 		db.session.add(newVetData)
 		db.session.commit()
 
 		# Pull price from db
-		if new_procedure == 'Neuter' or new_procedure == 'Spay (OHE)':
-			if new_animal_type == 'dog':
-				if new_weight == '0':
+		if newVetData.procedure == 'Neuter' or newVetData.procedure == 'Spay (OHE)':
+			if newVetData.animal_type == 'dog':
+				if newVetData.weight == '0':
 					weightString = "<25 Pound Dog"
-				elif new_weight == '1':
+				elif newVetData.weight == '1':
 					weightString = "25-50 Pound Dog"
-				elif new_weight == '2':
+				elif newVetData.weight == '2':
 					weightString = "51-75 Pound Dog"
 				else:
 					weightString = ">75 Pound Dog"
 				price = vetprocedure.query \
-							.filter_by(procedure = new_procedure) \
-							.filter_by(animal = new_animal_type.title()) \
+							.filter_by(procedure = newVetData.procedure) \
+							.filter_by(animal = newVetData.animal_type.title()) \
 							.filter_by(details = weightString) \
 							.first()
 			else:
-				price = vetprocedure.query.filter_by(procedure = new_procedure).filter_by(animal = new_animal_type.title()).first()
+				price = vetprocedure.query.filter_by(procedure = newVetData.procedure).filter_by(animal = newVetData.animal_type.title()).first()
 		else:
-			price = vetprocedure.query.filter_by(procedure = new_procedure).first()
+			price = vetprocedure.query.filter_by(procedure = newVetData.procedure).first()
 
-		return render_template('results.html', price=price)
+		# Calculate percentile
+		if float(newVetData.price) < price.suburban_median:
+			std = (price.suburban_median - price.suburban_25th_percentile) / 0.67449
+		else:
+			std = (price.suburban_75th_percentile - price.suburban_median) / 0.67449
+		
+		z_score = (float(newVetData.price) - price.suburban_median) / std
+		percentile = cdf(z_score)*100
 
+		percentileData = {
+			'25thPercentile': price.suburban_25th_percentile,
+			'median'        : price.suburban_median,
+			'75thPercentile': price.suburban_75th_percentile,
+			'userPercentile': int(percentile),
+			'procedure'     : newVetData.procedure
+		}
+
+		return render_template('results.html', procedure = newVetData.procedure, price = newVetData.price, percentile = percentile, percentileData = json.dumps(percentileData))
+
+@app.route('/interstitial', methods=['POST'])
+def interstitial():	
+	# Store their price (shhhhh)
+	global newVetData
+
+	new_animal_type = request.form['cat_dog']
+	new_price = request.form['cost']
+	new_weight = request.form.get('weight')
+	new_procedure = request.form['procedure']
+	new_zip = request.form['zip code']
+	new_clinic_name = request.form['vet_name']
+	new_clinic_yelp_id = request.form['vet_id']
+	newVetData = input_prices(
+		new_animal_type,
+		new_procedure,
+		new_price,
+		new_zip,
+		new_clinic_name,
+		new_clinic_yelp_id,
+		new_weight
+	)
+	db.session.add(newVetData)
+	db.session.commit()
+	return render_template('interstitial.html')
 
 # Routes for AJAX
 @app.route('/_get_clinics_in_zipcode', methods=['GET'])
 def get_clinics_in_zipcode():
-	zipcode = request.args.get('zipcode', 0, type=int)
-	latlng = Geocoder.geocode(zipcode)
+	latitude = request.args.get('latitude', 0, type=str)
+	longitude = request.args.get('longitude', 0, type=str)
 
 	# Session setup
 	session = rauth.OAuth1Session(
@@ -148,8 +203,12 @@ def get_clinics_in_zipcode():
 
 	params = {}
 	params["term"] = "veterinarian"
-	params["radius_filter"] = "40233"
-	params["ll"] = str(latlng.latitude) + ',' + str(latlng.longitude)
+	params["radius_filter"] = "16093" #10 miles
+	params["ll"] = str(latitude) + ',' + str(longitude)
+	params["limit"] = "20"
+	params["category_filter"] = "vet"
+	print str(latitude) + ',' + str(longitude)
+	params["sort"] = "1"
 
 	data = session.get('http://api.yelp.com/v2/search/', params=params) #returns response object, which we now name "data"
 	clinics_in_radius = data.json() #extracts json content from response object
@@ -157,7 +216,14 @@ def get_clinics_in_zipcode():
 	session.close()
 	return clinics_in_radius
 
-	
+@app.route('/_submit_email')
+def submit_email():
+	email_address = request.args.get('email', 0, type=str)
+	new_customer = waitlist(email_address)
+	db.session.add(new_customer)
+	db.session.commit()
+	return jsonify(email_address=email_address)
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
--- ismyvetrippingmeoff/static/js.js
+++ ismyvetrippingmeoff/static/js.js
@@ -1,54 +1,137 @@
+// Fade in weight text box if "dog" radio button is selected and a surgery is selected
+function weightFadeIn() {
+    var $animalRadio = $('input[name=cat_dog]:checked');
+    var $procedureSelect = $('select[name=procedure] option:selected'); 
 
-
-//Fade in weight text box if "dog" radio button is selected 
-$("#dog_selector").change(function() {
-    console.log("dog_selector changed")
-    if ($(this).prop("checked")){
-        $("#weight").fadeIn();
-        console.log("fade in")
+    if ($animalRadio.val() == 'dog' && $procedureSelect.parent().attr('label') == 'Surgery') {
+        $('#weight').fadeIn();
     }
+    else {
+        $('#weight').fadeOut();
+    }
+}
+
+// Bind function to change for animal radio and procedure select
+$('input[name=cat_dog]').change(function() {
+    weightFadeIn();
 });
 
-//Fade out weight text box if "cat" radio button is selected.
-$("#cat_selector").change(function() {
-    console.log("cat_selector changed")
-    if ($(this).prop("checked")){
-        $("#weight").fadeOut();
-        console.log("fade out")
-    }
+$('select[name=procedure]').change(function() {
+    weightFadeIn();
 });
 
+var geocoder;
 //Ajax function to populate the vet clinic dropdown based on zip code field.
 $('#zipcode').keyup(function() {
+    $("#zip_error").remove();
+    console.log("fade out");
     if($('#zipcode').val().length>=5){
-      $.getJSON($SCRIPT_ROOT + '/_get_clinics_in_zipcode', {
-        zipcode: $('#zipcode').val(),
-      }, function(data) {
+        //Here I will use google's javascript geocoder API to get the latlng of the given zipcode, and then I will send that to the server via AJAX
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { 'address':$('#zipcode').val(), 'componentRestrictions':{'postalCode':$('#zipcode').val()} }, function(results, status) {
+        
+        if (status == google.maps.GeocoderStatus.OK) {
+            result_latitude = results[0]['geometry']['location'].lat();
+            result_longitude = results[0]['geometry']['location'].lng();
+            console.log("First result latitude is: " + results[0]['geometry']['location'].lat());
+            console.log("First result longitude is: " + results[0]['geometry']['location'].lng());
+            //AJAX call:
+            $.getJSON($SCRIPT_ROOT + '/_get_clinics_in_zipcode', {
+                latitude: result_latitude,
+                longitude: result_longitude
+                }, function(data) {
+                    $('#vetname_drpdwn')
+                        .find('option')
+                        .remove();
 
-        $('#vetname_drpdwn')
-            .find('option')
-            .remove();
+                    //for each business in the yelp json response, passed here from "get_clinics_in_zipcode" function in flask:
+                    if(data['error']!=undefined){
+                        $("#zip_error").remove();
+                        $("#zip_group").append("<p id='zip_error' type='text' style='color:red;''>Sorry, that didn't work. It may be an invalid zip code. Please try a new zip code!</p>");
+                        console.log("fade in");
+                        $("#vetname_drpdwn").attr("disabled", "true");
+                    }
+                    else{
+                        for (i=0; i<data['businesses'].length; i++){
+                            $("#vetname_drpdwn")
+                                .append($("<option></option>")
+                                .attr("value", data['businesses'][i]['id'])
+                                .text(data['businesses'][i]['name']))
+                                .removeAttr("disabled");
+                        }
+                    }
+            });
+            return false;
 
-        //for each business in the yelp json response, passed here from "get_clinics_in_zipcode" function in flask:
-        if(data['error']!=undefined){
-            $("#zip_error").css("visibility","visible");
-            console.log("fade in");
-        }
-        else{
-            $("#zip_error").css("visibility","hidden");
-            console.log("fade out");
-            for (i=0; i<data['businesses'].length; i++){
-                $("#vetname_drpdwn")
-                    .append($("<option></option>")
-                    .attr("value", data['businesses'][i]['id'])
-                    .text(data['businesses'][i]['name']));
-            }
-        }
-      });
-      return false;
+          } else { //status of google.maps.GeocoderStatus is not OK
+            alert("Geocode was not successful for the following reason: " + status);
+          }
+        });
     }
 });
 
+$('document').ready(function(){
+    $('#emailSubmitButton').click(function(){
+        $.getJSON($SCRIPT_ROOT + '/_submit_email', {
+        email: $('#email_address_input').val(),
+        }, function(data) {
+            $('#email_address_input').val('');
+            $("#signup_confirmation").remove();
+            $("#email_group").append("<p id='signup_confirmation' style='color:green;'>Thanks for signing up! We'll be in touch :)</p> ");
+        });
+        return false;
+    })
+    $('#email_address_input').keypress(function(e){
+        if(e.which == 13){  //Enter key pressed
+            $('#emailSubmitButton').click();    //Trigger search button click event
+        }
+    });
+
+});
+
+// Guage
+function gauge(percentileData) {
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.0', {'packages':['gauge']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawGauge);
+
+    // Actually draw it!
+    function drawGauge() {
+        // Create and populate the data table.  Storing 0 initially so we get fun animation.
+        var data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['You', 0]
+        ]);
+
+        var sum = percentileData['25thPercentile'] + percentileData['median'] + percentileData['75thPercentile'];
+        var borderOne = percentileData['25thPercentile'] / sum * 100;
+        var borderTwo = borderOne + percentileData['median'] / sum * 100;
+
+        var options = {
+            greenFrom: 0, greenTo: borderOne,
+            yellowFrom: borderOne, yellowTo: borderTwo,
+            redFrom: borderTwo, redTo: 100,
+            minorTicks: 5,
+            animation:{
+                duration: 1000,
+                easing: 'out',
+            }
+        };
+
+        // Create and draw the visualization.
+        var gauge = new google.visualization.Gauge($('#gauge')[0]);
+        gauge.draw(data, options);
+
+        // Populate data with real value, redraw gauge.
+        data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['You', percentileData['userPercentile']]
+        ]);
+        gauge.draw(data, options);   
+    }
+}
 
 //This code does an action when typing stops for "delay" number of milliseconds. Not used.
 /*
