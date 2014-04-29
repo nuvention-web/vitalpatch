df7eb5eace105830f7201b05d291418a3fad960c
Glenn Fellman <GlennFellman2014@u.northwestern.edu>
2014-04-29 14:07:21 -0500

    fixed $ thing

--- ismyvetrippingmeoff.py
+++ ismyvetrippingmeoff.py
@@ -132,7 +132,7 @@ def interstitial():
 		global newVetData
 
 		new_animal_type = request.form['cat_dog']
-		new_price = request.form['cost']
+		new_price = request.form['cost'].strip('$')
 		new_weight = request.form.get('weight')
 		new_procedure = request.form['procedure']
 		new_zip = request.form['zip code']
